86fe4ce8a04d7059e3a1613a493ed1f4552f2dfd
Fabien Serny <fabien@23prod.fr>
2014-06-03 14:10:18 +0200

    Delete useless variable

--- views/js/displayBackOfficeHeader-1.5.js
+++ views/js/displayBackOfficeHeader-1.5.js
@@ -18,19 +18,18 @@
  */
 
 // Init var
-var froggypricenegociator_displayed = 0;
 var froggypricenegociator_current_price_field = '#finalPrice';
 
 $(document).ready(function() {
 
     // When document is ready, if merchant click on link price tab, we add the price negociator fields
     $('#link-Prices').click(function() {
-        if (froggypricenegociator_displayed == 0)
+
+        if ($('#froggypricenegociator-option').length == 0)
         {
             $('#tr_unit_price').parent().parent().after(fc_pn_negociator_options);
             froggyPriceNegociatorUpdate();
         }
-        froggypricenegociator_displayed = 1;
 
         // If price is updated, we update percent
         $('#priceTE').keydown(function() {
@@ -49,4 +48,5 @@ $(document).ready(function() {
         // Init
         froggyPriceNegociatorInit();
     });
+
 });

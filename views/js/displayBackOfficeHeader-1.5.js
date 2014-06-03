fae3597f6fa6c2ba654488b176de01249d517e4a
Fabien Serny <fabien@23prod.fr>
2014-06-03 15:56:29 +0200

    Dynamize product attribute configuration form for PS 1.5

--- views/js/displayBackOfficeHeader-1.5.js
+++ views/js/displayBackOfficeHeader-1.5.js
@@ -19,6 +19,7 @@
 
 // Init var
 var froggypricenegociator_current_price_field = '#finalPrice';
+var froggypricenegociator_current_price_combination_field = '#attribute_new_total_price';
 
 $(document).ready(function() {
 
@@ -56,13 +57,39 @@ $(document).ready(function() {
     });
 
     // When document is ready, if merchant click on link price tab, we add the price negociator fields
-    $('#link-Combinations').click(function() {
+    $('#product-tab-content-Combinations').click(function() {
 
         if ($('#froggypricenegociator-combination-option').length == 0)
         {
             var tmp = fc_pn_negociator_options.replace(/id="froggypricenegociator-/g, 'id="froggypricenegociator-combination-');
             $('#tr_unit_impact').parent().parent().after(tmp);
             $('#froggypricenegociator-combination-option-label').text(fc_pn_negociator_label_product_attribute);
+
+            // Init display and binding
+            setTimeout(function() {
+
+                // Init display
+                froggyPriceNegociatorInit(true);
+                froggyPriceNegociatorUpdate(true);
+
+                // If price is updated, we update percent
+                $('#attribute_price_impact').keydown(function() {
+                    froggyPriceNegociatorUpdate(true);
+                });
+                $('#attribute_price').keydown(function() {
+                    froggyPriceNegociatorUpdate(true);
+                });
+                $('#attribute_priceTI').keydown(function() {
+                    froggyPriceNegociatorUpdate(true);
+                });
+                $('#froggypricenegociator-combination-price-min').keydown(function() {
+                    froggyPriceNegociatorUpdate(true);
+                });
+                $('#froggypricenegociator-combination-reduction-percent-max').keydown(function() {
+                    froggyPriceNegociatorUpdate(true);
+                });
+
+            }, 500);
         }
 
     });
--- views/js/displayBackOfficeHeader-common.js
+++ views/js/displayBackOfficeHeader-common.js
@@ -17,33 +17,44 @@
  *  @copyright  2013-2014 Froggy Commerce
  */
 
-function froggyPriceNegociatorUpdate()
+function froggyPriceNegociatorUpdate(combination)
 {
+    // Set combination identifier, if we are on a combination edition
+    var combination_identifier = '';
+    if (combination)
+        combination_identifier = 'combination-';
+
+    // Get current price
+    var current_price = $(froggypricenegociator_current_price_field).val();
+    if (!current_price)
+        current_price = $(froggypricenegociator_current_price_field).text()
+    if (combination)
+    {
+        var current_price = $(froggypricenegociator_current_price_combination_field).val();
+        if (!current_price)
+            current_price = $(froggypricenegociator_current_price_combination_field).text()
+    }
+
     // Wait a few milliseconds (wait for the tab to be displayed), then calcul percent reduction
     setTimeout(function() {
-
-        // Get current price
-        var current_price = $(froggypricenegociator_current_price_field).val();
-        if (!current_price)
-            current_price = $(froggypricenegociator_current_price_field).text()
 
         if (fc_pn_type == 'PRICE_MINI')
         {
             // If "PRICE_MINI" is the configuration
-            var price_min = $('#froggypricenegociator-price-min').val();
+            var price_min = $('#froggypricenegociator-' + combination_identifier + 'price-min').val();
             var reduction_percent_max = 100 - (price_min * 100 / current_price);
             reduction_percent_max = Math.round(reduction_percent_max * 100) / 100;
-            $('#froggypricenegociator-reduction-percent-max').html('- ' + reduction_percent_max + '%');
-            $('#froggypricenegociator-reduction-percent-max-hidden').val(reduction_percent_max);
+            $('#froggypricenegociator-' + combination_identifier + 'reduction-percent-max').html('- ' + reduction_percent_max + '%');
+            $('#froggypricenegociator-' + combination_identifier + 'reduction-percent-max-hidden').val(reduction_percent_max);
         }
         else
         {
             // IF "PERCENT" is the configuration
-            var reduction_percent_max = $('#froggypricenegociator-reduction-percent-max').val();
+            var reduction_percent_max = $('#froggypricenegociator-' + combination_identifier + 'reduction-percent-max').val();
             var price_min = current_price * ((100 - reduction_percent_max) / 100);
             price_min = Math.round(price_min * 100) / 100;
-            $('#froggypricenegociator-price-min').html(price_min + ' ' + fc_pn_currency_sign);
-            $('#froggypricenegociator-price-min-hidden').val(price_min);
+            $('#froggypricenegociator-' + combination_identifier + 'price-min').html(price_min + ' ' + fc_pn_currency_sign);
+            $('#froggypricenegociator-' + combination_identifier + 'price-min-hidden').val(price_min);
         }
 
         // If general option is enabled
@@ -51,33 +62,43 @@ function froggyPriceNegociatorUpdate()
         {
             var price_min = current_price * ((100 - fc_pn_general_reduction) / 100);
             price_min = Math.round(price_min * 100) / 100;
-            $('#froggypricenegociator_minimum_sell_price').text(price_min + ' ' + fc_pn_currency_sign);
+            $('#froggypricenegociator-' + combination_identifier + 'minimum-sell-price').text(price_min + ' ' + fc_pn_currency_sign);
         }
 
     }, 200);
 }
 
-function froggyPriceNegociatorOptionStatus()
+function froggyPriceNegociatorOptionStatus(combination)
 {
+    // Set combination identifier, if we are on a combination edition
+    var combination_identifier = '';
+    if (combination)
+        combination_identifier = 'combination-';
+
     // If general option is enabled
     if (fc_pn_enable_general_option == 1)
     {
-        $('#froggypricenegociator-checkbox-details').fadeOut(500);
-        $('#froggypricenegociator-details').fadeOut(500);
+        $('#froggypricenegociator-' + combination_identifier + 'checkbox-details').fadeOut(500);
+        $('#froggypricenegociator-' + combination_identifier + 'details').fadeOut(500);
         return false;
     }
 
     // Else depending if option is enabled for this product
-    if ($('#froggypricenegociator-option').is(':checked'))
-        $('#froggypricenegociator-details').fadeIn(500);
+    if ($('#froggypricenegociator-' + combination_identifier + 'option').is(':checked'))
+        $('#froggypricenegociator-' + combination_identifier + 'details').fadeIn(500);
     else
-        $('#froggypricenegociator-details').fadeOut(500);
+        $('#froggypricenegociator-' + combination_identifier + 'details').fadeOut(500);
     return false;
 }
 
-function froggyPriceNegociatorInit()
+function froggyPriceNegociatorInit(combination)
 {
-    froggyPriceNegociatorOptionStatus();
-    $('#froggypricenegociator-option').click(function() { froggyPriceNegociatorOptionStatus(); });
+    // Set combination identifier, if we are on a combination edition
+    var combination_identifier = '';
+    if (combination)
+        combination_identifier = 'combination-';
+
+    froggyPriceNegociatorOptionStatus(combination);
+    $('#froggypricenegociator-' + combination_identifier + 'option').click(function() { froggyPriceNegociatorOptionStatus(combination); });
 }
 
